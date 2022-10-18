e33b2f73aa42549c1028220f93e493ee5415f890
Ivan <ivanteslenkopraha@gmail.com>
2022-10-17 20:52:00 -0400

    Blender Export tutorial

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,23 +1,11 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 import * as dat from "dat.gui"
 
-import squareSpace from "../static/square_space.jpg"
 import starsTexture from "../img/stars.jpg"
-import earthTexture from "../img/earth.jpg"
-import jupiterTexture from "../img/jupiter.jpg"
-import marsTexture from "../img/mars.jpg"
-import mercuryTexture from "../img/mercury.jpg"
-import neptuneTexture from "../img/neptune.jpg"
-import plutoTexture from "../img/pluto.jpg"
-import saturnRingTexture from "../img/saturn ring.png"
-import saturnTexture from "../img/saturn.jpg"
-import sunTexture from "../img/sun.jpg"
-import uranusRingTexture from "../img/uranus ring.png"
-import uranusTexture from "../img/uranus.jpg"
-import venusTexture from "../img/venus.jpg"
 
-
+const doggoUrl = new URL("../assets/doggo2.glb", import.meta.url)
 
 const renderer = new THREE.WebGLRenderer()
 
@@ -32,7 +20,7 @@ const scene = new THREE.Scene()
 const aspectRatio = window.innerWidth / window.innerHeight
 
 const camera = new THREE.PerspectiveCamera(
-    100,
+    45,
     aspectRatio,
     0.1,
     1000
@@ -49,11 +37,14 @@ camera.position.set(-10, 50, 30)
 const ambientLight = new THREE.AmbientLight(0x333333)
 scene.add(ambientLight)
 
+// const directLight = new THREE.DirectionalLight(0xffffff, 0.8)
+// scene.add(directLight)
+// directLight.position.set(10,10,10)
+
 
 const cubeTextureLoader = new THREE.CubeTextureLoader()
-scene.background = cubeTextureLoader.load([starsTexture, starsTexture, starsTexture, starsTexture, starsTexture, starsTexture])
-
-orbits.update()
+// scene.background = cubeTextureLoader.load([starsTexture, starsTexture, starsTexture, starsTexture, starsTexture, starsTexture])
+renderer.setClearColor(0xffffff, 1)
 
 const gui = new dat.GUI()
 
@@ -69,94 +60,37 @@ const options = {
 
 const textureLoader = new THREE.TextureLoader()
 
-const createPlanet = (size, texture, position, ring) => {
-    const geo = new THREE.SphereGeometry(size, 30, 30)
-    const mat = new THREE.MeshStandardMaterial({
-        map: textureLoader.load(texture)
+const assetLoader = new GLTFLoader()
+
+const grid = new THREE.GridHelper(30, 30)
+scene.add(grid)
+
+
+let mixer;
+assetLoader.load(doggoUrl.href, (gltf) => {
+    const model = gltf.scene
+    scene.add(model)
+    mixer = new THREE.AnimationMixer(model)
+    const clips = gltf.animations
+    // const clip = THREE.AnimationClip.findByName(clips, "HeadAction")
+    // const action = mixer.clipAction(clip)
+    // action.play()
+    clips.forEach((clip) => {
+        const action = mixer.clipAction(clip)
+        action.play()
     })
-    const mesh = new THREE.Mesh(geo, mat)
-    const obj = new THREE.Object3D()
-    obj.add(mesh)
-    if (ring) {
-        const ringGeo = new THREE.RingGeometry(ring.innerRadius, ring.outerRadius, 32)
-        const ringMat = new THREE.MeshBasicMaterial({
-            map: textureLoader.load(ring.texture),
-            side: THREE.DoubleSide,
-        })
-        const ringMesh = new THREE.Mesh(ringGeo, ringMat)
-        obj.add(ringMesh)
-        ringMesh.position.x = position
-        ringMesh.rotation.x = -0.5 * Math.PI
-    }
-    scene.add(obj)
-    mesh.position.x = position
-    return { mesh, obj }
-}
 
-// Sun
-const sunGeo = new THREE.SphereGeometry(16, 30, 30)
-const sunMat = new THREE.MeshBasicMaterial({
-    map: textureLoader.load(sunTexture)
-})
-const sun = new THREE.Mesh(sunGeo, sunMat)
-scene.add(sun)
-
-
-// Mercury
-const mercury = createPlanet(3.2, mercuryTexture, 28)
-
-const venus = createPlanet(5.8, venusTexture, 44)
-const earth = createPlanet(6, earthTexture, 62)
-const mars = createPlanet(4, marsTexture, 78)
-const jupiter = createPlanet(12, jupiterTexture, 100)
-
-
-
-// Saturn
-const saturn = createPlanet(10, saturnTexture, 138, {
-    innerRadius: 10,
-    outerRadius: 20,
-    texture: saturnRingTexture
+}, undefined, (error) => {
+    console.error(error)
 })
 
-const uranus = createPlanet(7, uranusTexture, 176, {
-    innerRadius: 7,
-    outerRadius: 12,
-    texture: uranusRingTexture
-})
-const neptune = createPlanet(7, neptuneTexture, 200)
-const pluto = createPlanet(2.8, plutoTexture, 216)
+orbits.update()
 
-
-const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300)
-scene.add(pointLight)
-
+const clock = new THREE.Clock()
 
 function animate(time) {
-
-    // Self-rotation
-    sun.rotateY(0.004)
-    mercury.mesh.rotateY(0.004)
-    venus.mesh.rotateY(0.002)
-    earth.mesh.rotateY(0.02)
-    mars.mesh.rotateY(0.018)
-    jupiter.mesh.rotateY(0.04)
-    saturn.mesh.rotateY(0.038)
-    uranus.mesh.rotateY(0.03)
-    neptune.mesh.rotateY(0.032)
-    pluto.mesh.rotateY(0.008)
-
-    //Around-sun-rotation
-    mercury.obj.rotateY(0.04);
-    venus.obj.rotateY(0.015);
-    earth.obj.rotateY(0.01);
-    mars.obj.rotateY(0.008);
-    jupiter.obj.rotateY(0.002);
-    saturn.obj.rotateY(0.0009);
-    uranus.obj.rotateY(0.0004);
-    neptune.obj.rotateY(0.0001);
-    pluto.obj.rotateY(0.00007);
-
+    // console.log(mixer)
+    mixer?.update(clock.getDelta())
     renderer.render(scene, camera)
 }
 
