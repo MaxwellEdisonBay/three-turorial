dafb73ed112d2f7481485b664f702ac5eeb21021
Ivan <ivanteslenkopraha@gmail.com>
2022-10-22 13:53:09 -0400

    Physics engine

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,6 @@
 import * as THREE from "three"
+import * as CANNON from "cannon-es"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import * as dat from "dat.gui"
-
-import space from "../assets/space.jpg"
 
 const renderer = new THREE.WebGLRenderer()
 
@@ -28,61 +26,141 @@ const orbits = new OrbitControls(camera, renderer.domElement)
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-camera.position.set(-5, 5, 5)
+camera.position.set(-5, 35, 35)
 
 
 const ambientLight = new THREE.AmbientLight(0x333333)
 scene.add(ambientLight)
 
-// const directLight = new THREE.DirectionalLight(0xffffff, 0.8)
-// scene.add(directLight)
-// directLight.position.set(10,10,10)
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8)
+scene.add(directionalLight)
+directionalLight.position.set(0, 50, 0)
 
 
-const cubeTextureLoader = new THREE.CubeTextureLoader()
-// scene.background = cubeTextureLoader.load([starsTexture, starsTexture, starsTexture, starsTexture, starsTexture, starsTexture])
-// renderer.setClearColor(0xffffff, 1)
+const mouse = new THREE.Vector2()
+const intersectionPoint = new THREE.Vector3()
+const planeNormal = new THREE.Vector3()
+const plane = new THREE.Plane()
+const raycaster = new THREE.Raycaster()
 
-const gui = new dat.GUI()
+window.addEventListener("click", (e) => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    planeNormal.copy(camera.position).normalize();
+    plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+    raycaster.setFromCamera(mouse, camera);
+    raycaster.ray.intersectPlane(plane, intersectionPoint);
+    const sphereMesh = new THREE.Mesh(
+        new THREE.SphereGeometry(1, 30, 30),
+        new THREE.MeshStandardMaterial({
+            color: 0xffea00,
+            metalness: 0,
+            roughness: 0
+        })
+    );
+    scene.add(sphereMesh);
+    sphereMesh.position.copy(intersectionPoint);
+});
 
-const options = {
-    sphereColor: "#ffea00",
-    wireframe: false,
-    speed: 0.01,
-    angle: 0.2,
-    penumbra: 0,
-    intensity: 1,
+const sphereGeo = new THREE.SphereGeometry(2)
+const sphereMat = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+})
+const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat)
+scene.add(sphereMesh)
 
-}
+const boxGeo = new THREE.BoxGeometry(2, 2, 2)
+const boxMat = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true
+})
+const boxMesh = new THREE.Mesh(boxGeo, boxMat)
+scene.add(boxMesh)
 
-const uniforms = {
-    u_time: { type: 'f', value: 0.0 },
-    u_resolution: { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight).multiplyScalar(window.devicePixelRatio) },
-    u_mouse: { type: 'v2', value: new THREE.Vector2(0.0, 0.0) },
-    image: {type: 't', value: new THREE.TextureLoader().load(space)}
-}
+const groundGeo = new THREE.PlaneGeometry(30, 30)
+const groundMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    wireframe: true,
+})
+const groundMesh = new THREE.Mesh(groundGeo, groundMat)
+scene.add(groundMesh)
 
-window.addEventListener('mousemove', (e) => {
-    uniforms.u_mouse.value.set(e.screenX / window.innerWidth, 1 - e.screenY / window.innerHeight)
+
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.81, 0)
 })
 
-const geometry = new THREE.PlaneGeometry(10, 10, 30, 30)
-const material = new THREE.ShaderMaterial({
-    vertexShader: document.getElementById("vertexShader").textContent,
-    fragmentShader: document.getElementById("fragmentShader").textContent,
-    // wireframe: true,
-    uniforms,
+const groundPhysMat = new CANNON.Material()
+
+const groundBody = new CANNON.Body({
+    // shape: new CANNON.Plane(),
+    shape: new CANNON.Box(new CANNON.Vec3(15, 15, 0.1)),
+    // mass: 10
+    type: CANNON.Body.STATIC,
+    material: groundPhysMat
 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+world.addBody(groundBody)
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+
+
+const boxPhysMat = new CANNON.Material()
+
+const boxBody = new CANNON.Body({
+    mass: 1,
+    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+    position: new CANNON.Vec3(1, 20, 0),
+    material: boxPhysMat,
+})
+world.addBody(boxBody)
+
+boxBody.angularVelocity.set(0, 10, 10)
+boxBody.angularDamping = 0.5
+
+const groundBoxContactMat = new CANNON.ContactMaterial(
+    groundPhysMat,
+    boxPhysMat,
+    { friction: 0 }
+)
+
+world.addContactMaterial(groundBoxContactMat)
+
+const spherePhysMat = new CANNON.Material()
+
+const sphereBody = new CANNON.Body({
+    mass: 10,
+    shape: new CANNON.Sphere(2),
+    position: new CANNON.Vec3(0, 15, 0),
+    material: spherePhysMat,
+})
+world.addBody(sphereBody)
+sphereBody.linearDamping = 0.31
+
+const groundSphereContactMat = new CANNON.ContactMaterial(
+    groundPhysMat,
+    spherePhysMat,
+    { restitution: 0.9 }
+)
+
+world.addContactMaterial(groundSphereContactMat)
 
 orbits.update()
 
-const clock = new THREE.Clock()
+const timeStep = 1 / 60
 
 function animate() {
+    world.step(timeStep)
 
-    uniforms.u_time.value = clock.getElapsedTime()
+    groundMesh.position.copy(groundBody.position)
+    groundMesh.quaternion.copy(groundBody.quaternion)
+
+    boxMesh.position.copy(boxBody.position)
+    boxMesh.quaternion.copy(boxBody.quaternion)
+
+    sphereMesh.position.copy(sphereBody.position)
+    sphereMesh.quaternion.copy(sphereBody.quaternion)
+
     renderer.render(scene, camera)
 }
 
